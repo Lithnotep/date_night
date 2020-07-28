a10767ec2e26161d616bb03486888f5962d95977
Max Mitrani <mjmitrani168@gmail.com>
2020-07-28 10:52:09 -0600

    add final changes will need to research nodes more

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,3 +1,14 @@
 class BinarySearchTree
-  
+  def initialize
+    @head = nil
+  end
+
+  def insert(rating, name)
+    depth = 0
+    if @head == nil
+      @head = Node.new(rating, name)
+    elsif rating < @head
+    end
+  end
+
 end
